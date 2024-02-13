eee42f709723eee892c90fcc1911ec0ba06c0fbc
Mduduzindlovu <Mduduzindlovu61@gmail.com>
2024-02-13 22:01:37 +0200

    Finished

--- counter/js/main.js
+++ counter/js/main.js
@@ -37,7 +37,7 @@ function initializeSite() {
 	})();
 
 	// Init effect 
-	$('#scene').parallax();
+	
 
 };
 /* END ------------------------------------------------------- */
@@ -61,10 +61,7 @@ $(document).ready(function() {
         render: function(data) {
             var el = $(this.el);
             el.empty()
-                .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
-                .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
-                .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
-                .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+			.append("<div><span><a href='../Valentines day/index.html'></a>Happy Valentines Day Baby</span></div>");
 
             // Check if the countdown has reached the end date
             if (data.total <= 0) {
